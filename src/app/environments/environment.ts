a18981b3479a25c680751dcd2c3a5eb1be8fe303
zeelias2906 <js.elias27@hotmail.com>
2025-05-12 21:56:00 -0300

    segundo comit

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    url: 'http://localhost:8080/mistura-boa/api/',
+    url: 'https://mistura-boa-api.onrender.com/mistura-boa/api/',
     DEFAULT_MODAL_OPTIONS: {
        centered: true,
     },
