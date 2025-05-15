da54bc1c3566ddad2295b838d0aae0154055a0fd
zeelias2906 <js.elias27@hotmail.com>
2025-05-14 22:08:44 -0300

    add netlify5

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://mistura-boa-back-i23x.onrender.com/mistura-boa/api'
+  apiUrl: 'https://mistura-boa-back-i23x.onrender.com/mistura-boa/api/'
 };
